24695122758be7c06a83bca07ee35a0f6c5c1d6b
Man Hoang Xuan <manhoangx@HarveyNash.vn.local>
2016-07-18 15:37:34 +0700

    QueryPParam: @QueryParam,  @Context UriInfo info, @DefaultValue

--- jax-rs/RESTfulExample/src/main/java/com/mkyong/rest/UserRestService.java
+++ jax-rs/RESTfulExample/src/main/java/com/mkyong/rest/UserRestService.java
@@ -1,8 +1,15 @@
 package com.mkyong.rest;
 
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/users")
 public class UserRestService {
@@ -15,6 +22,81 @@ public class UserRestService {
 	@Path("/vip")
 	public Response getUserVIP() {
 		return Response.status(200).entity("getUserVIP is called").build();
+	}
+	
+	@GET
+	@Path("{name}")
+	public Response getUserByName(@PathParam("name")String name) {
+		return Response.status(200).entity("getUserByName is called, name: " + name).build();
+	}
+	
+	@GET
+	@Path("{id : \\d+}") //support digit only
+	public Response getUserById(@PathParam("id") String id) {
+
+	   return Response.status(200).entity("getUserById is called, id : " + id).build();
+
+	}
+	
+	@GET
+	@Path("/username/{username : [a-zA-Z][a-zA-Z_0-9]}")
+	public Response getUserByUserName(@PathParam("username") String username) {
+
+	   return Response.status(200)
+		.entity("getUserByUserName is called, username : " + username).build();
+
+	}
+	
+	@GET
+	@Path("/books/{isbn : \\d+}")
+	public Response getUserBookByISBN(@PathParam("isbn") String isbn) {
+
+	   return Response.status(200)
+		.entity("getUserBookByISBN is called, isbn : " + isbn).build();
+
+	}
+	
+	@GET
+	@Path("/query")
+	public Response getUsers(
+		@QueryParam("from") int from,
+		@QueryParam("to") int to,
+		@QueryParam("orderBy") List<String> orderBy) {
+
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+
+	}
+	
+	@GET
+	@Path("/query1")
+	public Response getUsers(@Context UriInfo info) {
+
+		String from = info.getQueryParameters().getFirst("from");
+		String to = info.getQueryParameters().getFirst("to");
+		List<String> orderBy = info.getQueryParameters().get("orderBy");
+		
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+
+	}
+	
+	@GET
+	@Path("/query2")
+	public Response getUsers1(
+		@DefaultValue("1000") @QueryParam("from") int from,
+		@DefaultValue("999")@QueryParam("to") int to,
+		@DefaultValue("name") @QueryParam("orderBy") List<String> orderBy) {
+
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+
 	}
 
 }
